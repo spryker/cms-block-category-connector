270621a15f32e94a9d50242a541da925c9091ee9
René Klatt <rene.klatt@spryker.com>
2017-08-04 05:54:53 +0000

    core-1652 fixed some tests

--- tests/SprykerTest/Zed/CmsBlockCategoryConnector/_support/CmsBlockCategoryConnectorBusinessTester.php
+++ tests/SprykerTest/Zed/CmsBlockCategoryConnector/_support/CmsBlockCategoryConnectorBusinessTester.php
@@ -18,10 +18,10 @@ use Codeception\Actor;
  *
  * @SuppressWarnings(PHPMD)
  */
-class BusinessTester extends Actor
+class CmsBlockCategoryConnectorBusinessTester extends Actor
 {
 
-    use _generated\BusinessTesterActions;
+    use _generated\CmsBlockCategoryConnectorBusinessTesterActions;
 
    /**
     * Define custom actions here
